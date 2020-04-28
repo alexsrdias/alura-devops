2039b5b95b1f7ce20b0fb6474c57a9d4b2d8c17b
Alex Dias <alexsrd@gmail.com>
2020-04-29 00:49:15 +0100

    Primeira rota curso flash alura

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
   return 'Hey, we have Flask in a Docker container!'
 
+@app.route('/inicio')
+def inicio():
+    return '<h1>ALEX DIAS</h1>'
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
