6648f38c9e4824a52c58b574fb6a1ef105c2ee1d
Alex Dias <alexsrd@gmail.com>
2020-04-29 02:14:10 +0100

    Classe Jogos

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+class Jogos:
+  def __init__(self, nome, categoria, console):
+    self.nome = nome 
+    self.categoria = categoria 
+    self.console = console
+
+
 @app.route('/')
 def hello_world():
   return 'Hey, we have Flask in a Docker container!'
 
 @app.route('/alura')
 def inicio():
-    lista = ['Tetris', 'Super Mario', 'Pokemon Gold']
+    jogo1 = Jogos('Super Mario', 'Acao', 'SNES')
+    jogo2 = Jogos('Dinossauro Cadilac', 'Aventura', 'ARCADE')
+    jogo3 = Jogos('Uncharted 4', 'Aventura', 'PS4')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('base.html', titulo='Jogos do Alex Dias', jogos=lista)
 
 if __name__ == '__main__':
