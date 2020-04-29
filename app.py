0aa906cd20bb2cd6bdd6fe97eece407c3c52ceb9
Alex Dias <alexsrd@gmail.com>
2020-04-29 01:52:26 +0100

    Lista de Jogos Dinamico

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def hello_world():
 
 @app.route('/alura')
 def inicio():
-    return render_template('base.html')
+    lista = ['Tetris', 'Super Mario', 'Pokemon Gold']
+    return render_template('base.html', titulo='Jogos do Alex Dias', jogos=lista)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0', port=9999)
