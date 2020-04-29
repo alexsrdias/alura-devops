5d479bb6525e9a7080139ef3bc6ab85932e41245
Alex Dias <alexsrd@gmail.com>
2020-04-29 06:28:36 +0100

    Tela de Login e Session

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'fcf69511f5b3c2006c8106472c7a485b'
 
 
 class Jogos:
@@ -20,19 +21,36 @@ lista = [jogo1, jogo2, jogo3]
 # Rota - index
 @app.route('/')
 def index():
-
+  
+  if 'usuario_logado' not in session or session['usuario_logado'] == None:
+    return redirect('/login')
+  else:
     return render_template('home.html', titulo='Jogos do Alex Dias', jogos=lista)
 
 # Rota de Novo Elemento
 @app.route('/novo')
 def novo():
-    return render_template('form.html', titulo='Novo Jogo')
-
+  
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+    else:
+      return render_template('form.html', titulo='Novo jogo')
 
 # Rota de Login
 @app.route('/login')
 def login():
-    return render_template('login.html')
+  
+  return render_template('login.html')
+
+# Rota de Logout
+@app.route('/logout')
+def logout():
+  
+  session ['usuario_logado']=None
+  
+  flash('Não está Logado')
+
+  return render_template('login.html')
 
 #
 # Rotas de Serviços
@@ -45,16 +63,33 @@ def criar():
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogos(nome, categoria, console)
+
     lista.append(jogo)
+
     return redirect('/')
   
 # Rota de Serviço de Atenticar  
 @app.route('/auth', methods=['POST'])
 def auth():
+
   if '123' == request.form['senha']:
+
+    session ['usuario_logado'] = request.form['usuario']
+    flash(request.form['usuario'] + ' logou com sucesso!')
+
     return redirect('/')
+
   else :
+
+    flash('Não logado, tente de novo!')
+
     return redirect ('/login')
+
+# Verificar se esta logado
+def check_login():
+
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=9999)
